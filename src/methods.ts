e6e7778d7cb334529b64f6afbbd767cdccf40d6a
Ethan Groene <ethangroene@gmail.com>
2023-10-21 00:51:34 -0600

    Convert src/methods.js to ts file

--- src/methods.ts
+++ src/methods.ts
@@ -1,4 +1,4 @@
-const shuffleArray = (array) => {
+const shuffleArray = (array: []): [] => {
   for (let i = 1; i < array.length; i++) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
